c3dc55d2395b45e8b8ecac0cd9a31de2b770a0ba
Ginny-Dang <ginnydang0728@gmail.com>
2022-06-28 16:03:31 -0500

    Complete the navigation

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+const nav = document.querySelector(".primary-navigation")
+const navToggle = document.querySelector(".mobile-nav-toggle")
+
+navToggle.addEventListener("click", () => {
+    // if the nav is closed, open it
+    const visibility = nav.getAttribute("data-visible")
+
+    // if the nav is open, close it
+    if (visibility === "false") {
+        nav.setAttribute("data-visible", true)
+        navToggle.setAttribute("aria-expanded", true)
+    } else {
+        nav.setAttribute("data-visible", false)
+        navToggle.setAttribute("aria-expanded", false)
+    }
+
+    // console.log(navToggle.getAttribute("aria-expanded"))
+})
+
+
+
